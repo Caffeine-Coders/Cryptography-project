4b2cbfb3aa29b3634bf5d55c2651f2793492f348
ForumShah8 <shahforu00@gmail.com>
2023-04-26 14:43:04 -0400

    encryption

--- src/ecc/encryption.jsx
+++ src/ecc/encryption.jsx
@@ -5,51 +5,75 @@ var b = 0x0000000000000000000000000000000000000000000000000000000000000007
 var G = (0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
     0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
 var n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
-// var private_key1 = Math.floor(Math.random()*n) +1;
-const private_key1 = 200;
-
-function pointAdd(P,Q){
-    const R = P.add(Q);
-    return [R.getX().toJSON(), R.getY().toJSON()];
+let private_key1 = Math.floor(Math.random()*n) +1;
+let public_key1 = point_multiplication(private_key1,G,p,a)
+function point_multiplication(n,P,p,a){
+    let Q = null
+    let R = P
+    while(n>0){
+        if(n%2 == 1){
+            Q = point_addition(Q,R,p,a)
+        }
+        R = point_doubling(R,p,a)
+        n = Math.floor(n/2)
+    }
+    return Q
 }
-function pointMul(k,P){
-    const R = P.mul(k);
-    return [R.getX().toJSON(), R.getY().toJSON()];
+function point_addition(P,Q,p,a){
+    if(P == null){
+        return Q
+    }
+    if (Q == null){
+        return P
+    }
+    if(P.x == Q.x && P.y != Q.y){
+        return null
+    }
+    let s;
+    if(P.x == Q.x && P.y == Q.y){
+        s = (3 * P.x * P.x +a)*mod_inverse(2*P.y,p)
+    }
+    else{
+        s = (Q.y - P.y) * mod_inverse(Q.x - P.x , p)
+    }
+    let x = (s*s - P.x - Q.x) % p
+    let y = (s * (P.x -x ) - P.y)%p
+    return {x:x,y: y}
 }
-// const tmp = BigInt(private_key1)
-
-// const public_key1 = Number(point_multiplication(private_key1,G,p,a))
-
-// function point_multiplication(k,P,p,a){
-// var Q=P
-// var R=null;
-// while(k>0){
-//     if(k%2 === 1){
-//         R = point_addition(R,Q,p,a) || Q;
-//     }
-//     Q = point_addition(Q,Q,p,a);
-//     k = Math.floor(k/2)
-// }
-// return R
-// }
-
-// function point_addition(P,Q,p,a){
-//     var m;
-//     if(P[0]==Q[0] && P[1]== Q[1]){
-//         m = (3 * Math.pow(P[0],2) + a) * Math.pow(2*P[1], -1, p)
-//     }
-//     else{
-//         m = (Q[1] - P[1])* Math.pow(Q[0] - P[0],-1,p)
-//     }
-//     let Result_x = (Math.pow(m,2) - P[0] - Q[0]) % p
-//     let Result_y = (P[1] + m * (Result_x - P[0])) % p
-//     return [Result_x, -Result_y%p]
-// }
+function point_doubling(P,p,a){
+    if(P==null){
+        return null
+    }
+    let s = (3* P.x * P.x + a) * mod_inverse(2*P.y,p)
+    let x = (s*s - 2*P.x)%p
+    let y = (s * (P.x - x ) - P.y) % p
+    return {x:x,y:y}
+}
+function mod_inverse(a,m){
+    let m0 = m
+    let x0 = 0, x1= 1
+    if(m==1){
+        return 0
+    }
+    while(a>1){
+        let q = Math.floor(a/m)
+        let t = m 
+        m = a % m
+        a = t
+        t = x0
+        x0 = x1 - q * x0
+        x1 = t
+    }
+    if(x1<0){
+        x1 += m0
+    }
+    return x1
+}
 return(
     <div>
         {private_key1}
         <br/>
-        {/* {public_key1} */}
+        [{public_key1.x},{public_key1.y}]
     </div>
 )
 
